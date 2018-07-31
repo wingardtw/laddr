70ebcceb7e4b161c9e45ae386de7e181237b7785
Taylor Wingard <twingard@raybeam.com>
2018-07-30 19:45:21 -0700

    initialize

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
