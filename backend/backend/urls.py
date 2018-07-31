2ab377dad4477f6776dc16ca4d3d2a15ab3c6646
Taylor Wingard <twingard@raybeam.com>
2018-07-30 20:04:58 -0700

    user serializer

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from api import views
 
 router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
