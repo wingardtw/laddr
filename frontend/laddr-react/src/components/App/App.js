2f9f39a63cc1385c3458f3af6e6ef9885d49d3c5
SplodeyHead <nikbusacc@gmail.com>
2018-08-05 23:50:20 -0700

    cleaner routing and some home page templating

--- frontend/laddr-react/src/components/App/App.js
+++ frontend/laddr-react/src/components/App/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from '../Navbar/Navbar';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "../Home/Home"
+import Teams from "../Teams/Teams"
+import Profile from "../Profile/Profile"
+import Navigation from '../Navigation/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-
+        <BrowserRouter>
+          <div className="wrapper">
+            <Navigation />
+            <div className="route__container">
+              <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/Teams" component={Teams} />
+                <Route path="/Profile" component={Profile} />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
