65b4aee86d84b0de1b6c284e54e0550d57992c55
SplodeyHead <nikbusacc@gmail.com>
2018-09-20 15:00:10 -0700

    restarting...lol

--- frontend/laddr-react/src/components/App/App.js
+++ frontend/laddr-react/src/components/App/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 
-import Home from "../Home/Home"
-import Teams from "../Teams/Teams"
-import Profile from "../Profile/Profile"
-import Navigation from '../Navigation/Navigation';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <div className="wrapper">
-            <Navigation />
-            <div className="route__container">
-              <Switch>
-                <Route path="/" component={Home} exact />
-                <Route path="/Teams" component={Teams} />
-                <Route path="/Profile" component={Profile} />
-              </Switch>
-            </div>
-          </div>
-        </BrowserRouter>
+
       </div>
     );
   }
