df4227730322bfaa6f95ca20977cfd0b49f2ce70
Taylor Wingard <twingard@raybeam.com>
2018-10-24 17:41:20 -0700

    stuff

--- backend/api/matching.py
+++ backend/api/matching.py
@@ -9,9 +9,9 @@ def exclude_matches(profile):
     """
     matches = Match.objects.filter(Q(player_a=profile) | Q(player_b=profile))
     matched_ids = [
-        *[x.player_a.uuid for x in matches],
+        *[x.player_a.uuid for x in matches],  # noqa: E999
         *[x.player_b.uuid for x in matches],
-    ]  # flake8: noqa
+    ]
     return matched_ids
 
 
@@ -32,3 +32,21 @@ def find_match_tier_2(profile):
     profiles = Profile.objects.exclude(uuid__in=matched_ids)
     # Match based on goal similarity
 
+
+def find_match_tier_3(profile):
+    """ Finds a profile to serve as a potential tier 3 match
+        @profile - api.models.Profile to be matched
+    """
+    matched_ids = exclude_matches(profile)
+    profiles = Profile.objects.exclude(uuid__in=matched_ids)
+    # Match based on calibration
+    x = [*['1'], *['2']]
+
+
+def find_match_tier_4(profile):
+    """ Finds a profile to serve as a potential tier 4 match
+        @profile - api.models.Profile to be matched
+    """
+    matched_ids = exclude_matches(profile)
+    profiles = Profile.objects.exclude(uuid__in=matched_ids)
+    # Match based on profile similarity
