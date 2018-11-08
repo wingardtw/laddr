c9926eec79cc0e28d7f2a9dc835beac811b75759
Taylor Wingard <twingard@raybeam.com>
2018-11-07 18:11:35 -0800

    expire match

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,8 +7,11 @@ from django.contrib.postgres.fields import JSONField
 from django.db import models
 from django.utils.timezone import now
 import uuid
+import datetime
 
 # Create your models here.
+DEFAULT_MATCH_DURATION_DAYS = 30
+DEFAULT_MATCH_EXPIRATION = now() + datetime.timedelta(days=DEFAULT_MATCH_DURATION_DAYS)
 
 LOL_SERVERS = (("NA", "North America"),)
 
@@ -25,6 +28,7 @@ ROLES = (
     ("Mid", "Mid"),
     ("Top", "Top"),
 )
+
 RANKS = (
     ("Bronze V", "Bronze V"),
     ("Bronze IV", "Bronze IV"),
@@ -56,9 +60,7 @@ RANKS = (
 
 
 class Profile(models.Model):
-    uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     summoner_name = models.CharField(max_length=50, null=True, blank=False)
     lol_server = models.CharField(
@@ -71,27 +73,25 @@ class Profile(models.Model):
         max_length=40, choices=PLAYSTYLES, default="Conservative"
     )
     top_champions = JSONField(
-        default={"First": None, "Second": None, "Third": None}
+        default=dict([("First", None), ("Second", None), ("Third", None)])
     )
     favorite_color = models.CharField(max_length=15, blank=False, null=True)
     bio = models.TextField(max_length=500, blank=True, null=False)
-    role = models.CharField(
-        max_length=15, choices=ROLES, null=True, blank=False
-    )
+    role = models.CharField(max_length=15, choices=ROLES, null=True, blank=False)
     availability = JSONField(
-        default={
-            "Monday": False,
-            "Tuesday": False,
-            "Wednesday": False,
-            "Thursday": False,
-            "Friday": False,
-            "Saturday": False,
-            "Sunday": False,
-        }
+        default=(
+            [
+                ("Monday", False),
+                ("Tuesday", False),
+                ("Wednesday", False),
+                ("Thursday", False),
+                ("Friday", False),
+                ("Saturday", False),
+                ("Sunday", False),
+            ]
+        )
     )
-    rank = models.CharField(
-        max_length=15, choices=RANKS, blank=False, null=True
-    )
+    rank = models.CharField(max_length=15, choices=RANKS, blank=False, null=True)
     is_real = models.BooleanField(default=True)
 
     johnny_rank = models.IntegerField(default=0)
@@ -115,18 +115,14 @@ class Profile(models.Model):
 
 
 class Membership(models.Model):
-    uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     team = models.ForeignKey("Team", on_delete=models.CASCADE)
     date_joined = models.DateTimeField(auto_now_add=True)
 
 
 class Team(models.Model):
-    uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -159,9 +155,9 @@ class PsychePreference(models.Model):
     accepted = models.BooleanField(default=False)
 
     def __str__(self):
-        return '{uname} {accept} t:{tr} j:{jr} s:{sr}'.format(
+        return "{uname} {accept} t:{tr} j:{jr} s:{sr}".format(
             uname=self.user.user.username,
-            accept='accepts' if self.accepted else 'rejects',
+            accept="accepts" if self.accepted else "rejects",
             tr=self.psychograph.timmy_rank,
             jr=self.psychograph.johnny_rank,
             sr=self.psychograph.spike_rank,
@@ -178,22 +174,34 @@ class Psychograph(models.Model):
     calibrator = models.BooleanField(default=False)
 
     class Meta:
-        unique_together = ('johnny_rank', 'timmy_rank', 'spike_rank')
+        unique_together = ("johnny_rank", "timmy_rank", "spike_rank")
 
 
-class Match(models.Model):
+class SuggestedMatch(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     player_a = models.ForeignKey(
-        "Profile",
-        on_delete=models.CASCADE,
-        related_name='player_a',
+        "Profile", on_delete=models.CASCADE, related_name="player_a"
     )
     player_b = models.ForeignKey(
-        "Profile",
-        on_delete=models.CASCADE,
-        related_name='player_b',
+        "Profile", on_delete=models.CASCADE, related_name="player_b"
     )
     player_a_accept = models.BooleanField(default=False)
     player_b_accept = models.BooleanField(default=False)
+    a_accept_at = models.DateTimeField(default=None, null=True)
+    b_accept_at = models.DateTimeField(default=None, null=True)
     created_at = models.DateTimeField(default=now)
     updated_at = models.DateTimeField(auto_now=True)
+    expires_at = models.DateTimeField(default=DEFAULT_MATCH_EXPIRATION)
+    partner = models.ForeignKey(
+        "DuoPartner", on_delete=models.DO_NOTHING, null=True, default=None
+    )
+
+
+class DuoPartner(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    player_a = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="player_a"
+    )
+    player_b = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name="player_b"
+    )
