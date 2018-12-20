14d96b3a0a5214333ce6e5cfbc51881acf808316
Taylor Wingard <twingard@raybeam.com>
2018-12-19 18:50:23 -0800

    purge models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from .timezones import TIMEZONE_CHOICES
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.db import models
@@ -13,9 +12,7 @@ from api.utility import default_match_expire
 from constants import (
     RANKS,
     ROLES,
-    SERVERS,
     PLAYSTYLES,
-    DOW,
 )
 
 import uuid
@@ -29,12 +26,6 @@ class Profile(models.Model):
     )
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     summoner_name = models.CharField(max_length=50, null=True, blank=True)
-    server = models.CharField(
-        max_length=10, blank=True, choices=SERVERS, default="NA"
-    )
-    timezones = models.CharField(
-        max_length=50, default="Etc/UTC", choices=TIMEZONE_CHOICES
-    )
     playstyle = models.CharField(
         max_length=40, choices=PLAYSTYLES, default="Conservative"
     )
@@ -44,17 +35,9 @@ class Profile(models.Model):
     role = models.CharField(
         max_length=15, choices=ROLES, null=True, blank=True
     )
-    availabilities = models.ManyToManyField("Availability")
     rank = models.IntegerField(choices=RANKS, blank=True, null=True)
     is_real = models.BooleanField(default=True)
 
-    johnny_rank = models.IntegerField(default=0)
-    spike_rank = models.IntegerField(default=0)
-    timmy_rank = models.IntegerField(default=0)
-    preferred_johnny_rank = models.IntegerField(default=0)
-    preferred_spike_rank = models.IntegerField(default=0)
-    preferred_timmy_rank = models.IntegerField(default=0)
-    num_profiles_ranked = models.IntegerField(default=0)
     endorsements = models.ManyToManyField(
         "Endorsement",
         through="Endorsements",
@@ -74,76 +57,6 @@ class Profile(models.Model):
             "Casual": self.timmy_rank,
         }
         return max(stats.keys(), key=(lambda key: stats[key]))
-
-
-class Membership(models.Model):
-    uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    team = models.ForeignKey("Team", on_delete=models.CASCADE)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-
-class Team(models.Model):
-    uuid = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, editable=False
-    )
-    name = models.CharField(max_length=128, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    members = models.ManyToManyField(User, through="Membership")
-    is_real = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Availability(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    player = models.ForeignKey("Profile", on_delete=models.CASCADE, null=True)
-    day_of_week = models.CharField(max_length=15, choices=DOW, blank=True, null=True)
-    time_of_day = models.TimeField(null=True)
-    duration = models.DurationField(null=True)
-    active = models.BooleanField(default=True)
-
-    def __str__(self):
-        date_format = "%A %H:%M"
-        return "%s - %s" % (
-            self.start.strftime(date_format),
-            self.end.strftime(date_format),
-        )
-
-
-class PsychePreference(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user = models.ForeignKey("Profile", on_delete=models.CASCADE)
-    psychograph = models.ForeignKey("Psychograph", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-    accepted = models.BooleanField(default=False)
-
-    def __str__(self):
-        return '{uname} {accept} t:{tr} j:{jr} s:{sr}'.format(
-            uname=self.user.user.username,
-            accept='accepts' if self.accepted else 'rejects',
-            tr=self.psychograph.timmy_rank,
-            jr=self.psychograph.johnny_rank,
-            sr=self.psychograph.spike_rank,
-        )
-
-
-class Psychograph(models.Model):
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-    johnny_rank = models.IntegerField(default=0)
-    spike_rank = models.IntegerField(default=0)
-    timmy_rank = models.IntegerField(default=0)
-    calibrator = models.BooleanField(default=False)
-
-    class Meta:
-        unique_together = ('johnny_rank', 'timmy_rank', 'spike_rank')
 
 
 class Connection(models.Model):
